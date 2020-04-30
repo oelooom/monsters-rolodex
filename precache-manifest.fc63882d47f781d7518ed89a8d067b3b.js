f153f5f769d6b85895d4d2d3f47e0b535e84e098
Bahrul Ulum <oelooom@Bahruls-MacBook-Pro.local>
2020-04-30 21:21:02 +0700

    Updates

---
+++ precache-manifest.fc63882d47f781d7518ed89a8d067b3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77302f6d78be6d9b0e904a56301d3300",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "581bddd59fcc22f5cbd9",
+    "url": "/monsters-rolodex/static/css/main.aee9d51a.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monsters-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "581bddd59fcc22f5cbd9",
+    "url": "/monsters-rolodex/static/js/main.ff25bdcb.chunk.js"
+  },
+  {
+    "revision": "46fcc5578023f1586c35",
+    "url": "/monsters-rolodex/static/js/runtime-main.7affd505.js"
+  }
+]);
